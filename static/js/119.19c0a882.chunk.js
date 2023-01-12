44490e6d9b4c706ee7221992f967a1a7f1f28088
alf767443 <alf767443@users.noreply.github.com>
2023-01-12 23:00:36 +0000

    deploy: bed03cdf064d3db1fc5aa5ebfba11563a1388ba1

---
+++ static/js/119.19c0a882.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkcedri_ugv_dashboard=self.webpackChunkcedri_ugv_dashboard||[]).push([[119],{11119:function(n,a,c){c.r(a);var r=c(82937),u=c(46417);a.default=function(){return(0,u.jsx)(r.ZP,{container:!0,rowSpacing:1.75,columnSpacing:2})}}}]);
