5a6d7267382b0e30b621ab731a0564bf43c045e9
alf767443 <andreluisfranca98+GitHub@gmail.com>
2023-01-21 14:09:16 +0000

    Battery -add carousel

--- src/graphs/battery/index.js
+++
@@ -1,5 +0,0 @@
-import { BatteryIcon } from "./batteryIcon"
-import { percentageDatetimeArea } from "./percentageDatetimeArea"
-
-export class BatteryIcon { BatteryIcon };
-export class percentageDatetimeArea { percentageDatetimeArea };
