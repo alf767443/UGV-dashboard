15a10f00a24350c323bb287d890bf8a488a4099d
alf767443 <andreluisfranca98+GitHub@gmail.com>
2023-01-24 11:34:00 +0000

    Bullet graphs add

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,8 +1,7 @@
 // material-ui
 import { Grid } from '@mui/material';
 import DemoHeatmap from 'graphs/heatmaps/posConnection';
-import CurrentBullet from 'graphs/motors/currentBullet';
-import { Stack } from '../../../node_modules/@mui/joy/index';
+import MotorBullets from 'graphs/motors/groupBullet';
 
 
 // project import
@@ -14,9 +13,7 @@ const DashboardDefault = () => {
         <Grid container rowSpacing={1.75} columnSpacing={2}>
             {/* Main block */}
             <DemoHeatmap />
-            <Stack direction={'column'} alignItems={'center'} spacing={0} >
-                <CurrentBullet />
-            </Stack>
+            <MotorBullets />
         </Grid>
     );
 };
