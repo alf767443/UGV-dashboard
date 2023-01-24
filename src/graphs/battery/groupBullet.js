81c81204a283df0dbde4ea927478157c4c43cbf0
alf767443 <andreluisfranca98+GitHub@gmail.com>
2023-01-24 10:25:43 +0000

    Bullet graphs add

---
+++ src/graphs/battery/groupBullet.js
@@ -0,0 +1,16 @@
+// material-ui
+import PercentageBullet from 'graphs/battery/percentageBullet';
+import CurrentBullet from 'graphs/battery/currentBullet';
+import VoltageBullet from 'graphs/battery/voltageBullet';
+import { Stack } from '../../../node_modules/@mui/joy/index';
+
+export default BatteryBullets = () => {
+    return (
+        <Stack direction={'column'} alignItems={'center'} spacing={0} >
+            <PercentageBullet />
+            <CurrentBullet />
+            <VoltageBullet />
+        </Stack>
+    );
+};
+
