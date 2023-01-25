e749f0019be3675f72360c70a37a493b8b01e3fe
alf767443 <andreluisfranca98+GitHub@gmail.com>
2023-01-25 13:58:10 +0000

    Add box

--- src/graphs/battery/groupBullet.js
+++ src/graphs/battery/groupBullet.js
@@ -1,17 +1,32 @@
 // material-ui
+import styles from 'graphs/styles';
+
+
 import PercentageBullet from 'graphs/battery/percentageBullet';
 import CurrentBullet from 'graphs/battery/currentBullet';
 import VoltageBullet from 'graphs/battery/voltageBullet';
-import { Stack } from '../../../node_modules/@mui/joy/index';
+import { Box, Typography, Stack } from '@mui/material';
+import MainCard from "components/MainCard";
 
 const BatteryBullets = () => {
     return (
-        <Stack direction={'column'} alignItems={'center'} spacing={0} >
-            <PercentageBullet />
-            <CurrentBullet />
-            <VoltageBullet />
-        </Stack>
+        <MainCard sx={styles.maincard.sx} content={styles.maincard.content}>
+            <Box sx={styles.box.sx}>
+                <Stack spacing={styles.stack.spacing} direction={styles.stack.direction} alignItems={styles.stack.alignItems}>
+                    <Typography variant={styles.typography.variant} color={styles.typography.color}>
+                        Battery actual states
+                    </Typography>
+                    <Stack direction={'column'} alignItems={'center'} spacing={0} >
+                        <PercentageBullet />
+                        <CurrentBullet />
+                        <VoltageBullet />
+                    </Stack>
+                </Stack>
+            </Box>
+        </MainCard>
     );
 };
 
 export default BatteryBullets;
+
+
