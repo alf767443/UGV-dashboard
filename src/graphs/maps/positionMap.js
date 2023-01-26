733bcbad98a7be3458aab3fd9b0f22df3643216a
alf767443 <andreluisfranca98+GitHub@gmail.com>
2023-01-26 15:55:54 +0000

    Map add

--- src/graphs/maps/positionMap.js
+++ src/graphs/maps/positionMap.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Heatmap } from '@ant-design/plots';
-import cedri from './heatmaps/cedri.png'
+import cedri from './images/cedri.png'
 import { url, requestOptions } from 'API/url';
+import styles from 'graphs/styles';
 
+import { Box, Typography, Stack } from '@mui/material';
+import MainCard from "components/MainCard";
 
 var rawPosition = JSON.stringify({
 	"dataSource": "CeDRI",
@@ -13,7 +16,8 @@ var rawPosition = JSON.stringify({
 			'$project': {
 				'dateTime': 1,
 				'x': 1,
-				'y': 1
+				'y': 1, 
+                'yaw': '$orient.yaw'
 			}
 		}, {
 			'$sort': {
@@ -23,8 +27,64 @@ var rawPosition = JSON.stringify({
 			'$limit': 1
 		}
 	]
-   });
+});
 
+var raw = JSON.stringify({
+    "dataSource": "CeDRI",
+	"database": "CeDRI_UGV_buffer",
+	"collection": "PositionAMCL_Data",
+	"pipeline": [
+        {
+            '$project': {
+                'dateTime': {
+                    '$dateTrunc': {
+                        'date': '$dateTime', 
+                        'unit': 'minute'
+                    }
+                }, 
+                'x': {
+                    '$toInt': '$x'
+                }, 
+                'y': {
+                    '$toInt': '$y'
+                }, 
+                'orient': 1
+            }
+        }, {
+            '$addFields': {
+                'xy': {
+                    '$concat': [
+                        {
+                            '$toString': [
+                                '$x'
+                            ]
+                        }, ':', {
+                            '$toString': [
+                                '$y'
+                            ]
+                        }
+                    ]
+                }
+            }
+        }, {
+            '$group': {
+                '_id': '$xy', 
+                'x': {
+                    '$avg': '$x'
+                }, 
+                'y': {
+                    '$avg': '$y'
+                }, 
+                'yaw': {
+                    '$avg': '$orient.yaw'
+                }, 
+                'count': {
+                    '$count': {}
+                }
+            }
+        },
+    ]
+})
 
 
 export default class ConnectivityIcon extends React.Component {
@@ -32,24 +92,25 @@ export default class ConnectivityIcon extends React.Component {
 		super(props);
 
 		this.state = {
-            data: [],
-            last: [],
+            data: [{x:-3520, y:-2960, cont:null},{x:3520, y:2960, count:null}],
+            last: {x:0,y:0,yaw:2},
 			ticks: -1,
 			quality: 0
         };
     }  
     
-    canUpdate(){
+    canUpdate(){ 
 		if (JSON.parse(window.localStorage.getItem('fromLocal')) || this.state.ticks <= 0) {
 			this.setState({ ticks: 10})
-			this.refreshList()
+			this.refreshPos();
 		} else if (!JSON.parse(window.localStorage.getItem('fromLocal'))){
 			// From MongoDB cloud
 			this.setState({ ticks: this.state.ticks - 1})
 		}
 	}
 
-    refreshList() {
+    refreshPos() {
+        // Last point
 		fetch(url(), requestOptions(rawPosition))
 		.then((response) => response.json())
 		.then((json) => {
@@ -60,9 +121,21 @@ export default class ConnectivityIcon extends React.Component {
 		});
     }
 
+    refreshMap(){
+        //History of points
+        fetch(url(), requestOptions(raw))
+        .then((response) => response.json())
+        .then((json) => {
+            this.setState({ data: [...json, {x:-3520, y:-2960, cont:null},{x:3520, y:2960, count:null}] });
+        })
+        .catch((error) => {
+            console.log(error)
+        });
+    }
+
     componentDidMount = () => {
-		this.refreshList();
-        this.asyncFetch();
+		//this.refreshMap();
+        this.refreshPos();
 		this.timer();
     }
 
@@ -75,28 +148,15 @@ export default class ConnectivityIcon extends React.Component {
 			this.canUpdate();
 		}, 1000)
 	}
-
-    asyncFetch = () => {
-        // Get last position
-        fetch('https://gw.alipayobjects.com/os/antvdemo/assets/data/heatmap.json')
-        .then((response) => response.json())
-        .then((json) => {
-            this.setState({data: json})
-            this.setState([{g:3520,l:2960,tmp:null}, {g:-3520,l:-2960,tmp:null},...this.state.data])
-        })
-        .catch((error) => {
-            console.log('fetch data failed', error);
-        });
-    };
   
     config = {
         type: 'density',
-        xField: 'g',
-        yField: 'l',
+        xField: 'x',
+        yField: 'y',
         xAxis: false,
         yAxis: false,
-        colorField: 'tmp',
-        //limitInPlot: true,
+        colorField: 'count',
+        limitInPlot: true,
         color: '#F51D27-#FA541C-#FF8C12-#FFC838-#FAFFA8-#80FF73-#12CCCC-#1890FF-#6E32C2',
         annotations: [
             {
@@ -104,22 +164,35 @@ export default class ConnectivityIcon extends React.Component {
                 start: [-3520, 2960],
                 end: [3520, -2960],
                 src: cedri,
-            }, {
-                type: 'dataMarker',
-                position: [100,100],
-            }
+            },
         ],
-        tooltips:{
-            fields: ['g','l']
-        },
     };
 
     render() {
-        console.log();
+        const position = {
+            type: 'dataMarker',
+            // src: UGV,
+            // style: {
+            //     width: 20,
+            //     height: 15,
+            // },
+            // start: [this.state.last.x,this.state.last.y],
+            // end: [this.state.last.x,this.state.last.y],
+            position: [this.state.last.x,this.state.last.y],
+            rotate: this.state.last.yaw,
+        }
         return(
-            <div>
-                <Heatmap {...this.config} data={this.state.data} sx={{width: 300, height: 300}} />
-            </div>
+            <MainCard sx={styles.maincard.sx} content={styles.maincard.content}>
+                <Box sx={styles.box.sx}>
+                    <Stack spacing={styles.stack.spacing} direction={styles.stack.direction} alignItems={styles.stack.alignItems}>
+                        <Typography variant={styles.typography.title.variant} color={styles.typography.title.color}>
+                            UGV position
+                        </Typography>
+                        <Heatmap {...this.config} data={this.state.data} sx={{width: 300, height: 300}} annotations={[...this.config.annotations, position]} />
+                    </Stack>
+                </Box>
+            </MainCard>
         );
     }
 }
+
