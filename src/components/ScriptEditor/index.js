37655d3d23d0ddec7bd4ad535ab9950bbc390e40
alf767443 <andreluisfranca98@gmail.com>
2023-06-01 13:45:53 +0100

    Script editor

--- src/components/ScriptEditor/index.js
+++ src/components/ScriptEditor/index.js
@@ -1,152 +1,296 @@
-/* eslint-disable no-unused-vars */
-import React, { Component  } from 'react';
-import * as echarts from 'echarts';
-import { djangoFetch } from 'API/url';
-import { useLocation } from 'react-router-dom';
+		/* eslint-disable no-unused-vars */
+		import React, { Component  } from 'react';
+		import { djangoFetch } from 'API/url';
+		import GridLayout from 'react-grid-layout';
+		import { MenuItem, Select, Button } from '@mui/material';
+
+		import { IconButton } from '@mui/material';
+		import { NoteAdd, Delete, Save } from '@mui/icons-material';
+		import { message } from 'antd';
+
+		import {
+			Typography,
+			Grid,
+			InputLabel,
+			OutlinedInput,
+		} from '@mui/material';
 
 
-import {
-	Typography,
-	Grid,
-    Button,
-	Box,
-    FormHelperText,
-    IconButton,
-    InputAdornment,
-    InputLabel,
-    OutlinedInput,
-    Stack,
-	Skeleton,
-	Divider,
-} from '@mui/material';
+		// third party
+		import * as Yup from 'yup';
+		import { Formik } from 'formik';
 
+		import MainCard from "components/MainCard";
 
-// third party
-import * as Yup from 'yup';
-import { Formik } from 'formik';
+		import PlotTile from 'components/Tiles/plotTile';
 
-import MainCard from "components/MainCard";
+		import Editor from 'react-simple-code-editor';
 
-import ReactPrismEditor from "react-prism-editor";
+		import Prism from 'prismjs';
+		import 'prismjs/components/prism-clike';
+		import 'prismjs/components/prism-python';
+		import 'prismjs/themes/prism.css'; //Example style, you can use another
 
-import SaveIcon from "@mui/icons-material/Save";
+		import "./styles.css";
 
+		const randomString = (length) => {
+			const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+			let result = '';
+			for (let i = length; i > 0; --i) {
+				result += chars[Math.floor(Math.random() * chars.length)];
+			}
+			return result;
+		}
 
-import styles from './styles';
-import "./styles.css";
+		export default class ScriptEditor extends React.Component {
+			constructor(props) {
+				super(props);
 
+				this.state = {
+					scriptID: this.getParam(),
+					script: null,
+					code: null,
+					next: null,
+					last: null,
+					sample: null,
+					status: null,
+					editor: false,
+					robotID: window.localStorage.getItem('robotID'),
+				};
+			}
 
-export default class ScriptEditor extends React.Component {
-	constructor(props) {
-		super(props);
+			addScriptClick = () => {
+				const messageID = randomString(5) + this.state.robotID
+				const sendJSON = {
+					"robot": this.state.robotID,
+					"code": "log(msg='Hello World', type='warn')",
+					"sample": 60,
+					"status": "stop",
+					"script": {
+						"title": "New Script",
+						"group": "New Script",
+					}
+				}
+				message.open({
+					key: messageID,
+					type: 'loading',
+					content: 'Creating a script',
+					duration: 0,
+				});
+				djangoFetch('/script', '/', 'POST', JSON.stringify(sendJSON))
+				.then((response) => {
+					if(response.status === 201){
+						this.getList();
+						this.render()
+					}
+				})
+				.then(() => message.open({
+					key: messageID,
+					type: 'success',
+					content: 'Script created',
+					duration: 2,
+				}))
+				.catch((e) => console.error(e))
+				.catch(() => message.open({
+					key: messageID,
+					type: 'error',
+					content: 'Error when creating',
+					duration: 2,
+				}))
+			}
 
-		this.state = {
-			graphID: this.props.graphID,
-			next: null,
-			last: null,
-			sample: null,
-			status: null, 
-			script: null,
-			code: null,
-			editor: false,
-		};
-	}
+			removeScriptClick = () => {
+				const messageID = randomString(5) + this.state.robotID
+				message.open({
+					key: messageID,
+					type: 'loading',
+					content: 'Deleting the script',
+					duration: 0,
+				});
+				djangoFetch('/script', '/?name='+this.state.scriptID, 'DELETE', '')
+				.then((response) => {
+					if(response.status === 301){
+						this.getList();
+						this.render();
+						window.location.href = `/CeDRI_dashboard/edit/script?id=null`;
+					}
+				})
+				.then(() => message.open({
+					key: messageID,
+					type: 'success',
+					content: 'Script deleted',
+					duration: 2,
+				}))
+				.catch((e) => console.error(e))
+				.catch(() => message.open({
+					key: messageID,
+					type: 'error',
+					content: 'Error when deleting',
+					duration: 2,
+				}))
+			}
 
-	getData(){
-		djangoFetch('/script', '/?name=' + this.state.graphID, 'GET', '')
-		.then(response => response.json())
-		.then((json) => {
-			const _json = json
-			this.setState(_json)
-			console.log(_json)
-		})
-		.then(() => console.log(this.state))
-		.catch((e) => console.error(e))
-	}
+			handleSubmitForm = () => {
+				const form = document.getElementById('Internal-Button-Save-InForm');
+				console.log(form.click())
+			}
 
-	componentDidMount = () => {
-		this.getData();
-	}
-
-	render() {
-		return (
-			<div className='main'>
-			<Grid
-				container
-				direction="row"
-				justifyContent="space-evenly"
-				alignItems="stretch"
-				columns={16}
-				spacing={1}
-			>					
-			{this.state.graphID && this.state.script?
-				<Grid item xs={16} sm={16} md={16} lg={16}>
-					<Formik
-						initialValues={{
-							name: this.state.script.title,
-							group: this.state.script.group,
-							submit: null,
-						}}
-						validationSchema={Yup.object().shape({
-							name: Yup.string().max(255).required('Robot is required'),
-							group: Yup.string().max(255).required('Robot is required'),
-						})}
-						onSubmit={async (values, { setErrors, setStatus, setSubmitting }) => {
-							const sendJSON = {
-								"filter": {'name': this.state.graphID},
-								"update": 
-									{
-										'$set': {
-											'script': {
-												'group': values.group,
-												'title': values.name
-											},
-											'code': this.state.code,
-										}
-									}
-							};
-							djangoFetch('/chart', '/?name='+this.state.graphID, 'PUT', JSON.stringify(sendJSON))
-								.then((response) => {
-									if (response.status === 202) {
-										setStatus({ success: true });
-										setSubmitting(false);
-									}
-									else {
-										setStatus({ success: false });
-										setErrors({ submit: 'Incorrect password or robot' });
-										setSubmitting(false);
-									}
-									return response.json();
-								})
-								.then(()=>this.getData())
-								.catch((e) => console.error(e));
-			
-							try {
-								setStatus({ success: false });
-								setSubmitting(false);
-							} catch (err) {
-								setStatus({ success: false });
-								setErrors({ submit: err.message });
-								setSubmitting(false);
+			getList = () => {
+				const sendJSON ={
+					"pipeline": [
+						{
+						'$match': {
+							'robot': this.state.robotID
+						}
+						}, {
+						'$group': {
+							'_id': '$name', 
+							'label': {
+								'$first': {
+									'$concat': [
+										'$script.group', ' - ', '$script.title'
+									]
+								}
 							}
-						} }
-					>
-						{({ errors, handleBlur, handleChange, handleSubmit, isSubmitting, touched, values }) => (
-							<form noValidate onSubmit={handleSubmit}>
-								<MainCard >
-									<Typography {...styles.typography.title}>
-										Chart configurations
-									</Typography>
+						}
+						}
+					]   
+				}
+				djangoFetch('/script', '/', 'OPTIONS', JSON.stringify(sendJSON))
+				.then((response) => response.json())
+				.then((json) => {
+					const _json = json
+					this.setState({list: _json})
+					console.log(_json)
+				})
+			}
+
+			handleGraphIDChange = (event) => {
+				this.setState({scriptID: event.target.value})
+				console.log(event)
+				window.location.href = `/CeDRI_dashboard/edit/script?id=${event.target.value}`;
+			}
+			
+			getParam = () => {
+				const searchParams = new URLSearchParams(location.search);
+				const scriptIDParam = searchParams.get('id');
+				console.log(scriptIDParam)
+				return scriptIDParam
+			}
+
+			componentBreakpoint = (trigger) =>{
+				const width = window.innerWidth
+				return width < trigger
+			}
+
+			getData = () =>{
+				djangoFetch('/script', '/?name=' + this.state.scriptID, 'GET', '')
+					.then(response => response.json())
+					.then((json) => {
+						const _json = json
+						this.setState(_json)	
+						this.setState({pipeline: JSON.stringify(_json.query.pipeline, null, '\t')})
+					})
+					.catch((e) => console.error(e))
+			}
+
+			componentDidMount = () => {
+				this.getList()
+				this.state.scriptID?this.getData():null
+			}
+
+			CodeEditor = () => {
+				return(
+				<MainCard style={{height:'100%', width:'100%'}}>
+					<Typography align='center' variant='h5' color='textSecondary' >
+						Script code
+					</Typography>
+					<Editor
+						value={this.state.code}
+						insertSpaces={1}
+						tabSize={1}
+						onValueChange={code => this.setState({code: code})}
+						highlight={code => Prism.highlight(code, Prism.languages.python, 'python')}
+						padding={10}
+						style={{
+							fontFamily: '"Fira code", "Fira Mono", monospace',
+							fontSize: 16,
+						}}
+						/>
+				</MainCard>
+			)}
+
+			FormEditor = () => {
+				return (
+					<MainCard style={{height:'100%', width:'100%'}}>
+						<Typography align='center' variant='h5' color='textSecondary' >
+							Script configurations
+						</Typography>
+						<Formik
+							initialValues={{
+								name: this.state.script.title,
+								group: this.state.script.group,
+								sample: this.state.sample,
+								submit: null,
+							}}
+							validationSchema={Yup.object().shape({
+								name: Yup.string().max(255).required('Robot is required'),
+								group: Yup.string().max(255).required('Robot is required'),
+								sample: Yup.number().required('Robot is required'),
+							})}
+							onSubmit={async (values, { setErrors, setStatus, setSubmitting }) => {
+								const sendJSON = {
+									"filter": {'name': this.state.scriptID},
+									"update": 
+										{
+											'$set': {
+												'script': {
+													'group': values.group,
+													'title': values.name
+												},
+												'sample': parseFloat(values.sample),
+												'code': this.state.code
+											}
+										}
+								};
+								djangoFetch('/script', '/?name='+this.state.scriptID, 'PUT', JSON.stringify(sendJSON))
+									.then((response) => {
+										if (response.status === 202) {
+											setStatus({ success: true });
+											setSubmitting(false);
+										}
+										else {
+											setStatus({ success: false });
+											setErrors({ submit: 'Incorrect password or robot' });
+											setSubmitting(false);
+										}
+										return response.json();
+									})
+									.then(()=>this.getData())
+									.catch((e) => console.error(e));
+
+								try {
+									setStatus({ success: false });
+									setSubmitting(false);
+								} catch (err) {
+									setStatus({ success: false });
+									setErrors({ submit: err.message });
+									setSubmitting(false);
+								}
+							}}
+						>
+							{({ errors, handleBlur, handleChange, handleSubmit, isSubmitting, touched, values }) => (
+								<form noValidate onSubmit={handleSubmit} style={{height:'100%', with:'100%'}} id='ScriptEditor-GridLayout-Grid-Form-form'>
 									<Grid
 										container
 										direction="row"
 										justifyContent="space-evenly"
 										alignItems="stretch"
-										columns={20}
+										columns={6}
 										spacing={2}
 									>
-										<Grid item xs={20} sm={20} md={20} lg={20}>
-											<InputLabel htmlFor="name">Chart name</InputLabel>
+										<Grid item xs={6} sm={6} md={2} lg={2}>
+											<InputLabel htmlFor="name">Script name</InputLabel>
 											<OutlinedInput
 												id="name"
 												type="name"
@@ -154,12 +298,12 @@ export default class ScriptEditor extends React.Component {
 												name="name"
 												onBlur={handleBlur}
 												onChange={handleChange}
-												placeholder="Chart name"
+												placeholder="Script name"
 												fullWidth
 												error={Boolean(touched.name && errors.name)} />
 										</Grid>
-										<Grid item xs={20} sm={20} md={20} lg={20}>
-											<InputLabel htmlFor="name">Chart group</InputLabel>
+										<Grid item xs={6} sm={6} md={2} lg={2}>
+											<InputLabel htmlFor="name">Script group</InputLabel>
 											<OutlinedInput
 												id="group"
 												type="group"
@@ -167,65 +311,98 @@ export default class ScriptEditor extends React.Component {
 												name="group"
 												onBlur={handleBlur}
 												onChange={handleChange}
-												placeholder="Chart group"
+												placeholder="Script group"
 												fullWidth
 												error={Boolean(touched.group && errors.group)} />
 										</Grid>
-									
-										<Grid
-											container
-											direction="row"
-											justifyContent="flex-end"
-											alignItems="stretch"
-											columns={20}
-											spacing={1}
-										>
-											<Grid item >
-												<Button
-													disableElevation
-													disabled={isSubmitting}
-													fullWidth
-													size="large"
-													type="submit"
-													variant="contained"
-													color="primary"
-													endIcon={<SaveIcon />}
-												>
-												Salvar
-												</Button>
-											</Grid>	
+										<Grid item xs={6} sm={6} md={2} lg={2}>
+											<InputLabel htmlFor="name">Sample time {`[s]`}</InputLabel>
+											<OutlinedInput
+												id="sample"
+												type="sample"
+												value={values.sample}
+												name="sample"
+												onBlur={handleBlur}
+												onChange={handleChange}
+												placeholder="Query sample"
+												fullWidth
+												error={Boolean(touched.sample && errors.sample)} />
 										</Grid>
+										<div className='scriptEditor-Form-Save'>
+											<Button
+												id='Internal-Button-Save-InForm'
+												disableElevation
+												disabled={isSubmitting}
+												fullWidth
+												type="submit"
+												variant="contained"
+												color="primary"
+											>
+											</Button>
+										</div>
 									</Grid>
-								</MainCard>
-							</form>
-						)}
-					</Formik>
-				</Grid>:<></>}
-			{this.state.graphID && this.state.code?
-				<Grid item xs={16} sm={16} md={16} lg={16}>
-					<MainCard>
-						<Typography {...styles.typography.title}>
-							Python script
-						</Typography>
-						<div >
-							<ReactPrismEditor
-								style={{ height: '100%', maxHeight: 'inherit' }}
-								language="python"
-								theme="tomorrow"
-								code={this.state.code}
-								lineNumber={true}
-								readOnly={false}
-								clipboard={true}
-								startingLineNumber={1}
-								changeCode={(code) => {
-									this.setState({ code: code });
-								} } />
-						</div>
-					</MainCard> 
-				</Grid>:<></>}
-			</Grid>
-			</div>
-		);
-	}
+								</form>	
+								)}
+						</Formik>
+					</MainCard>
+				)
+			}
 
-}
+			render() {
+				console.log(this.state)
+				const activeColor = '#454545';
+				return (
+					<div className='scriptEditor' id='ScriptEditor'>
+						<div className="Header" id='ScriptEditor-Header'>
+							<div className="SelectGraph" id='ScriptEditor-Header-SelectGraoh'>
+								<Select
+									value={this.state.scriptID}
+									onChange={this.handleGraphIDChange}
+									label="Script"
+									id="select-graph"
+									sx={{width: 200, height:36, backgroundColor: "#f5f5f5"}}
+								>
+									{this.state.list?this.state.list.map((graph) => (
+									<MenuItem key={graph._id} value={graph._id}>
+										{graph.label}
+									</MenuItem>
+									)):<></>}
+								</Select>
+							</div>
+							
+							<div className='Add' id='ScriptEditor-Header-AddButton'>
+								<IconButton sx={{ flexShrink: 0, backgroundColor: 'grey.100', color:'', height:36, width:36, borderRadius:2}}  onClick={this.addScriptClick} >
+									<NoteAdd sx={{color:activeColor, width:'130%' , height: '130%'}} />
+								</IconButton>
+							</div>
+
+							<div className='Remove' id='ScriptEditor-Header-RemoveButton'>
+								<IconButton sx={{ flexShrink: 0, backgroundColor: 'grey.100', color:'', height:36, width:36, borderRadius:2}}  onClick={this.removeScriptClick} >
+									<Delete sx={{color:activeColor, width:'130%' , height: '130%'}} />
+								</IconButton>
+							</div>
+							
+							<div className='Save' id='ScriptEditor-Header-SaveButton'>
+								<IconButton sx={{ flexShrink: 0, backgroundColor: 'grey.100', color:'', height:36, width:36, borderRadius:2}} onClick={this.handleSubmitForm}>
+									<Save sx={{color:activeColor, width:'130%' , height: '130%'}}/>
+								</IconButton>
+							</div>
+						</div>
+						<Grid container
+							padding={2}
+							rowSpacing={2}
+							direction="column"
+							justifyContent="flex-start"
+							alignItems="stretch">
+							<Grid item>
+								{this.state.script && this.state.sample?this.FormEditor():<MainCard />}
+							</Grid>
+							<Grid item>
+								{this.state.code?this.CodeEditor():<MainCard />}
+							</Grid>
+						</Grid>
+					</div>
+				);
+			}
+
+		}
--- src/pages/editScript/index.js
+++ src/pages/editScript/index.js
@@ -1,263 +1,28 @@
 import React from "react";
 // import ReactDOM from 'react-dom';
-import { MenuItem, Select } from '@mui/material';
 import ScriptEditor from "components/ScriptEditor/index";
-import { djangoFetch } from "API/url";
-import { message } from 'antd';
 
 // import { EditOutlined } from '@mui/icons-material';
-import { IconButton } from '@mui/material';
-import { Addchart, Delete } from '@mui/icons-material';
 
 import "./styles.css";
 
-const randomString = (length) => {
-	const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-	let result = '';
-	for (let i = length; i > 0; --i) {
-		result += chars[Math.floor(Math.random() * chars.length)];
-	}
-	return result;
-}
+
 
 export default class EditPage extends React.Component {
 	constructor(props) {
 		super(props);
 
         this.state = {
-            selectedScriptID: this.getParam(),
+            selectedGraphID: null,
             list: null,
             robotID: window.localStorage.getItem('robotID'),
         }
     }
 
-    addChartClick = () => {
-        const messageID = randomString(5) + this.state.robotID
-        const sendJSON = {
-            "query": {
-                "dataSource": "DataSource",
-                "database": "CeDRI_UGV_datalake",
-                "collection": "Battery",
-                "pipeline": [
-                {
-                    "$project": {
-                    "dateTime": {
-                        "$dateTrunc": {
-                        "date": "$dateTime",
-                        "unit": "minute"
-                        }
-                    },
-                    "percentage": 1
-                    }
-                },
-                {
-                    "$densify": {
-                    "field": "dateTime",
-                    "range": {
-                        "step": 1,
-                        "unit": "minute",
-                        "bounds": "full"
-                    }
-                    }
-                },
-                {
-                    "$group": {
-                    "_id": "$dateTime",
-                    "percentage": {
-                        "$avg": "$percentage"
-                    }
-                    }
-                },
-                {
-                    "$sort": {
-                    "_id": -1
-                    }
-                },
-                {
-                    "$limit": 180
-                },
-                {
-                    "$project": {
-                    "percentage": {
-                        "$multiply": [
-                        "$percentage",
-                        100
-                        ]
-                    }
-                    }
-                },
-                {
-                    "$sort": {
-                    "_id": 1
-                    }
-                }
-                ]
-            },
-            "option": "option = {\n      title: {\n        text: 'New Script',\n        left: '1%'\n      },\n      tooltip: {\n        trigger: 'axis'\n      },\n      grid: {\n        left: '5%',\n        right: '15%',\n        bottom: '10%'\n      },\n      xAxis: {\n        data: data.map(function (item) {\n          return item['_id'];\n        })\n      },\n      yAxis: {},\n      toolbox: {\n        right: 10,\n        feature: {\n          dataZoom: {\n            yAxisIndex: 'none'\n          },\n          restore: {},\n          saveAsImage: {}\n        }\n      },\n      dataZoom: [\n        {\n          startValue: '2014-06-01'\n        },\n        {\n          type: 'inside'\n        }\n      ],\n      visualMap: {\n        top: 50,\n        right: 10,\n        pieces: [\n          {\n            gt: 0,\n            lte: 20,\n            color: '#DB0700'\n          },\n          {\n            gt: 20,\n            lte: 40,\n            color: '#F58B0A'\n          },\n          {\n            gt: 40,\n            lte: 60,\n            color: '#EDD900'\n          },\n          {\n            gt: 60,\n            lte: 80,\n            color: '#7DD600'\n          },\n          {\n            gt: 80,\n            lte: 100,\n            color: '#1AB80F'\n          }\n        ],\n        outOfRange: {\n          color: '#999'\n        }\n      },\n      series: {\n        name: 'Percentage',\n        type: 'line',\n        data: data.map(function (item) {\n          return item['percentage'];\n        })\n      }\n    }\n",
-            "tile": {
-                "group": "New Group",
-                "title": "New Script"
-            },
-            "robot": this.state.robotID 
-        }
-        message.open({
-      top: Infinity,
-style: {zIndex: Infinity},
-
-			key: messageID,
-			type: 'loading',
-			content: 'Creating a script',
-			duration: 0,
-		});
-        djangoFetch('/script', '/', 'POST', JSON.stringify(sendJSON))
-        .then((response) => {
-            if(response.status === 201){
-                this.getList();
-                this.render()
-            }
-        })
-        .then(() => message.open({
-      top: Infinity,
-style: {zIndex: Infinity},
-
-			key: messageID,
-			type: 'success',
-			content: 'Script created',
-			duration: 2,
-        }))
-        .catch((e) => console.error(e))
-        .catch(() => message.open({
-      top: Infinity,
-style: {zIndex: Infinity},
-
-			key: messageID,
-			type: 'error',
-			content: 'Error when creating',
-			duration: 2,
-        }))
-    }
-
-    removeChartClick = () => {
-        const messageID = randomString(5) + this.state.robotID
-        message.open({
-      top: Infinity,
-style: {zIndex: Infinity},
-
-			key: messageID,
-			type: 'loading',
-			content: 'Deleting the script',
-			duration: 0,
-		});
-        djangoFetch('/script', '/?name='+this.state.selectedScriptID, 'DELETE', '')
-        .then((response) => {
-            if(response.status === 301){
-                this.getList();
-                this.render();
-                window.location.href = `/CeDRI_dashboard/edit/script?id=null`;
-            }
-        })
-        .then(() => message.open({
-      top: Infinity,
-style: {zIndex: Infinity},
-
-			key: messageID,
-			type: 'success',
-			content: 'Script deleted',
-			duration: 2,
-        }))
-        .catch((e) => console.error(e))
-        .catch(() => message.open({
-      top: Infinity,
-style: {zIndex: Infinity},
-
-			key: messageID,
-			type: 'error',
-			content: 'Error when deleting',
-			duration: 2,
-        }))
-    }
-
-    getList = () => {
-        const sendJSON ={
-            "pipeline": [
-                {
-                '$match': {
-                    'robot': this.state.robotID
-                }
-                }, {
-                '$group': {
-                    '_id': '$name', 
-                    'label': {
-                        '$first': {
-                            '$concat': [
-                                '$script.group', ' - ', '$script.title'
-                            ]
-                        }
-                    }
-                }
-                }
-            ]   
-        }
-        djangoFetch('/script', '/', 'OPTIONS', JSON.stringify(sendJSON))
-          .then((response) => response.json())
-          .then((json) => {
-            const _json = json
-            this.setState({list: _json})
-          })
-    }
-
-    handleGraphIDChange = (event) => {
-        this.setState({selectedScriptID: event.target.value})
-        console.log(event)
-        window.location.href = `/CeDRI_dashboard/edit/script?id=${event.target.value}`;
-    };
-
-    componentDidMount = () => {
-        this.getList()
-    }
-    
-    getParam = () => {
-        const searchParams = new URLSearchParams(location.search);
-        const graphIDParam = searchParams.get('id');
-        console.log(graphIDParam)
-        return graphIDParam
-    }
-
     render(){
-        const activeColor = '#454545';
         return (
-            <div className="main" id="EditorMainPage">
-                <div className="Header">
-                    <div className="SelectGraph">
-                        <Select
-                            value={this.state.selectedScriptID}
-                            onChange={this.handleGraphIDChange}
-                            label="Script"
-                            id="select-graph"
-                            sx={{width: 200, height:36, backgroundColor: "#f5f5f5"}}
-                        >
-                            {this.state.list?this.state.list.map((graph) => (
-                            <MenuItem key={graph._id} value={graph._id}>
-                                {graph.label}
-                            </MenuItem>
-                            )):<></>}
-                        </Select>
-                    </div>
-                    
-                    <div className='Add'>
-                        <IconButton sx={{ flexShrink: 0, backgroundColor: 'grey.100', color:'', height:36, width:36, borderRadius:2}}  onClick={this.addChartClick} >
-                            <Addchart sx={{color:activeColor, width:'130%' , height: '130%'}} />
-                        </IconButton>
-                    </div>
-
-                    <div className='Remove'>
-                        <IconButton sx={{ flexShrink: 0, backgroundColor: 'grey.100', color:'', height:36, width:36, borderRadius:2}}  onClick={this.removeChartClick} >
-                            <Delete sx={{color:activeColor, width:'130%' , height: '130%'}} />
-                        </IconButton>
-                    </div>
-                </div>
-                
-                {this.state.selectedScriptID?<div className="layout" id={this.state.selectedScriptID+"ScriptEditor"}><ScriptEditor graphID={this.state.selectedScriptID} /></div>:<></>}
+            <div className="main" id="EditorMainPage">                
+                <ScriptEditor graphID={this.state.selectedGraphID} />
             </div>
         );
     }
