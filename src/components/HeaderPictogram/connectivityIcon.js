0b08b6d6a5788cf08a5e5b69193f2d17f2c4c707
alf767443 <andreluisfranca98@gmail.com>
2023-06-17 10:41:59 +0100

    Remove log, add class and Id

--- src/components/HeaderPictogram/connectivityIcon.js
+++ src/components/HeaderPictogram/connectivityIcon.js
@@ -5,12 +5,6 @@ import React from "react";
 import { djangoFetch } from 'API/url';
 import { SignalWifi1Bar, SignalWifi2Bar, SignalWifi3Bar, SignalWifi4Bar,  SignalWifiStatusbarConnectedNoInternet4, SignalWifiOff } from '@mui/icons-material';
 
-const iconStyle = {
-	width: 30,
-	height: 30,
-	color: 'gray'
-}
-
 export default class ConnectivityIcon extends React.Component {
 	constructor(props) {
 		super(props);
@@ -50,8 +44,6 @@ export default class ConnectivityIcon extends React.Component {
 
 	quality = () => {
 		const status = this.state.status
-		console.log(status)
-		console.log((Date.now() - Date.parse(status.lastCheck)) < 20000)
 		if (status.is_alive && (Date.now() - Date.parse(status.lastCheck)) < 20000){
 			if (status.rtt_avg < 1){
 				this.setState({ quality:  1 });
@@ -80,55 +72,52 @@ export default class ConnectivityIcon extends React.Component {
 		switch(this.state.quality){
 			case 0:
 				return '#B3B3B3';
-				
 			case 1:
 				return '#0DB200';
-				
 			case 2:
 				return '#87DB00';
-				
 			case 3:
 				return '#F0F000';
-				
 			case 4:
 				return '#EB5801';
-				
 			case 5:
 				return '#e6afaf';
-
 			case 6:
 				return '#B60000';
-
 			default:
 				return '#B3B3B3'
-
 		}
 	}
 
 	connectIcon = () => {
+		const iconStyle = {
+			width: 30,
+			height: 30,
+			color:this.color()
+		}
 		switch(this.state.quality){
 			case 0:
-				return <SignalWifiOff sx={{...iconStyle, color:this.color()}} />
+				return <SignalWifiOff sx={{...iconStyle }} />
 			case 1:
-				return <SignalWifi4Bar sx={{...iconStyle, color:this.color()}} />
+				return <SignalWifi4Bar sx={{...iconStyle }} />
 			case 2:
-				return <SignalWifi3Bar sx={{...iconStyle, color:this.color()}} />
+				return <SignalWifi3Bar sx={{...iconStyle }} />
 			case 3:
-				return <SignalWifi2Bar sx={{...iconStyle, color:this.color()}} />
+				return <SignalWifi2Bar sx={{...iconStyle }} />
 			case 4:
-				return <SignalWifi1Bar sx={{...iconStyle, color:this.color()}} />
+				return <SignalWifi1Bar sx={{...iconStyle }} />
 			case 5:
-				return <SignalWifi4Bar sx={{...iconStyle, color:this.color()}} />
+				return <SignalWifi4Bar sx={{...iconStyle }} />
 			case 6:
-				return <SignalWifiStatusbarConnectedNoInternet4 sx={{...iconStyle, color:this.color()}} />
+				return <SignalWifiStatusbarConnectedNoInternet4 sx={{...iconStyle }} />
 			default:
-				return <SignalWifiOff sx={{...iconStyle, color:this.color()}} />
+				return <SignalWifiOff sx={{...iconStyle }} />
 		}
 	}
 
 	render() {
 		return (
-			<div>
+			<div className="connectionPictogram" id="Header-Pictograms-Connection">
 				{this.connectIcon()}
 			</div>
 		);
